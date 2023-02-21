d065eab88e0eaac7fd4c4afd4ad92431878c7f75
EdwinSch <edwinschutjes@gmail.com>
2023-02-21 14:43:33 +0100

    add products display and text filter logic

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
 /* ---- TARGETS && INITIALIZERS ---- */
+let filteredProducts = [...products];
+const productsContainer = document.querySelector(".products-container");
 
 /* ---- FUNCTIONS ---- */
 
-console.log("hello world");
+// Display Products Function
+function displayProducts() {
+  // conditional
+
+  productsContainer.innerHTML = filteredProducts
+    .map(({ id, title, image, price }) => {
+      return ` <article class="product" data-id='${id}'>
+          <img
+            class="product-img img"
+            src="${image}"
+            alt="product"
+          />
+          <footer>
+            <h5 class="product-name">${title}</h5>
+            <span class="product-price">&euro; ${price}</span>
+          </footer>
+        </article>`;
+    })
+    .join("");
+}
+//invoke function
+displayProducts();
+
+// Text Filter
+const form = document.querySelector(".input-form");
+const searchInput = document.querySelector(".search-input");
+
+form.addEventListener("keyup", function () {
+  const inputValue = searchInput.value;
+
+  filteredProducts = products.filter((product) => {
+    return product.title.toLowerCase().includes(inputValue);
+  });
+  // re-invoke display products function (new state)
+  displayProducts();
+});
